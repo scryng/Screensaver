466a2ae7463b0e4c102d84de5d4932387a85ea18
scryng <125830775+scryng@users.noreply.github.com>
2024-05-16 23:01:48 -0300

    feat: Initial settings and added button for test Hello World apparition.

--- src/JavaFXMain.java
+++ src/JavaFXMain.java
@@ -1,5 +1,12 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import javafx.scene.control.Button;
 
 public class JavaFXMain extends Application {
     public static void main(String[] args) {
@@ -8,6 +15,26 @@ public class JavaFXMain extends Application {
 
     @Override
     public void start(Stage primaryStage){
+        Button btn = new Button("Test");
+        Text text = new Text("Hello world");
+        text.setVisible(false);
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello world!");
+                text.setVisible(!text.isVisible());
+            }
+        });
 
+        HBox root = new HBox();
+
+        root.getChildren().add(btn);
+        root.getChildren().add((text));
+
+        Scene scene = new Scene(root, 500,500);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
     }
 }
